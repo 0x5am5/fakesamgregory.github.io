ffdc907802b28eaf18142a3755dc9b9342230d56
Sam Gregory <samuelgregory@me.com>
2015-04-26 16:06:33 +0100

    SG: First Commit

---
+++ development/index.php
@@ -0,0 +1,219 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<title>Sam Gregory - Digital Developer/Producer</title>
+<link href="css/main-sg.css" rel="stylesheet" type="text/css" />
+
+<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0">
+
+<meta name="description" content="I'm Sam Gregory an Digital Developer/Producer based on London. This is my portfolio website. Here are showcased all my works: websites, theatre, production, and more... ">
+<meta name="keywords" content="Web, developer, Digital, Media, HTML5, PHP, Javascript, Producer, Production Manager, website, graphics, framestore, rathergood.com, ilm">
+<link rel="shortcut icon" href="http://thegloriousdead.in/samgregory/images/favicon.ico" />
+</head>
+
+<script src="//ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min.js"></script>
+<script src="js/sg.js"></script>
+
+<body>
+
+<div class="container">
+
+  <div class="contact">
+    <ul id="contact" class="menu">
+      <li>Phone: <a href="tel:+447816061943">+44 7816 061 943</a></li>
+      <li>Email: <a href="mailto:samuelgregory@me.com?subject=Hi, I found you on your website">samuelgregory@me.com</a></li>
+  </div>
+
+  <header>
+    <div class="header">
+      <img src="images/head-logo-small.png" alt="sg logo" height="174" width="174"><span class="access"><h1>Sam Gregory - Digital Developer</h1></span>
+      <div class="menu">
+        <ul>
+          <li>
+            <a href="#who">Who I am</a>
+          </li>
+          <li>
+            <a href="#what">What I know</a>
+          </li>
+          <li>
+            <a href="#exp">What I've done</a>
+          </li>
+          <li>
+            <a href="#contact">Contact</a>
+          </li>
+      </div> 
+    </div>
+  </header>
+  
+  <div class="content">
+	  
+    <ul class="information">
+      <li id="who" class="intro section">
+          Hi there,
+          <p>
+            I’m an experienced digital media developer specialising in HTML, CSS and Javascript but with a broad knowledge in many disciplines. 
+            From 2009 to 2012 leveraged and honed these skills on key projects within 
+            <a href="http://www.framestore.com" target="_blank">Framestore’s</a> design and digital portfolio. 
+            It has been a hugely rewarding experience spanning interactive, VFX, motion graphics, titles and credits.
+          <p>
+          <p>
+            In addition to my work at Framestore, my interest in live performance which i achieved from being lead vocalist and guitarist in my band led me to co-create 
+            <a href="http://www.velvettrumpet.co.uk" title="Velvet Trumpet Theatre" target="_blank">Velvet Trumpet</a>, a theatre company in which I am Production Manager. 
+            I was thrilled to be at the helm of our first production “A Model Life”, which ran at The Network Theatre to rave reviews.
+          </p>
+          <p>
+            I combine technical aptitude with practicality and a client-driven, results-oriented approach. 
+            I enjoy the challenge of innovating and delighting my clients, while working within the realities of time and financial considerations.
+          </p>
+      </li><!-- END WHO SECTION -->
+
+      <li id="what" class="what section">
+        What I Know,
+        <p>
+          <strong>HTML, CSS, Javascript, Actionscript, Adobe CS6 (Fl, Ai, Ae, Dw, Ps, Pr)</strong>
+        </p>
+
+        <h2>Personal Skills</h2>
+        <ul>
+          <li>Confident in front of large groups of people</li>
+          <li>Composed under pressure</li>
+          <li>Positive work ethnic</li>
+          <li>Extremely hard working and dedicated</li>
+          <li>Try and get the most out of people</li>
+          <li>Strong desire to inspire</li>
+          <li>Love to project manage a teams schedule</li>
+        </ul>
+
+        <h2>University</h2>
+        <ul>
+          <li>ATRium University  October 2006 – July 2009</li>
+          <li>BA (Hons) Design for Media</li>
+        </ul>
+      </li><!-- END WHAT SECTION IN LIST-->
+          
+      <li id="exp" class="experience section">
+        What I've Done
+        <ul class="employer">
+          <li>
+            <p>
+              <h3>Framestore</h3> 2009 - 2012
+              <br />
+              <i>Flash Designer/Developer</i>
+            </p>
+            <p>
+              Initially employed as a Flash Designer, my role quickly diversified to a diverse portfolio including HTML, PHP, MySQL development, 
+              as well as graphic work and animation using Photoshop, illustrator, and After Effects. 
+              Increasingly relied upon for my technical ability, I was a well known generalist to my colleagues. 
+            </p>
+            
+            <ul class="work">
+              <li>
+                <strong><a href="http://taxi.ba.com/" title="Take a BA plane down your street!" target="_blank">BA ‘Take a Taxi Down Your Street’</a></strong> 
+                - (Flash/Actionscript/Photoshop)
+              </li>
+              <li>
+                <strong><a href="https://vimeo.com/45193293" title="Secret Cinema Promethius trailer" target="_blank">Secret Cinema’s Promethius</a></strong> 
+                - (After Effects/Illustrator)
+              </li>
+              <li>
+                <strong><a href="http://framestoredigital.com/2010/12/09/lg-3d-tv/" title="Ask Tim Key anything about 3D TV... and more!" target="_blank">LG 'Rare Butterflies' Web app</a></strong>
+                 - (Flash/Actionscript)
+              </li>
+              <li>
+                <strong><a href="http://uk.imdb.com/title/tt1204340/" title="Tyrannosaur front and end credits" target="_blank">Tyrannosaur Front/End Titles</a></strong> 
+                - (After Effects)
+              </li>
+            </ul><!-- END WORK LIST -->
+          </li><!-- END FRAMESTORE EMPLOYER -->
+
+          <li>
+            <p>
+              <h3>Velvet Trumpet</h3> March  2012 - Present
+              <br>
+              <i>Production Manager</i>
+            </p>
+            <p>
+              Founded this year, Velvet Trumpet is an “extracurricular” project where I have utilised the project management skills I have acquired in the course of my “day job”. 
+              I am responsible for organising rehearsal and performance space, managing company finances and fundraising, workshop management, liaison with suppliers and partners, live production. 
+              Because of my vast list of contacts I have gained as clients and work colleagues I aim to get things done as inexpensive as possible. 
+              I also aim to recruit people with a genuine love of what they do, hoping to provide them with a great line on their CV and future liaison. 
+              Velvet Trumpet also expand into comedy sketches in-which I organise and gather equipment to film and record audio with. 
+              I also arrange location, dates and times where actors and technical staff are required, as well as the post production if necessary. 
+              Such as VFX, sound, and editing.
+            </p>
+            <ul>
+              <li><strong><a href="http://velvettrumpet.co.uk/amodellife.php" title="A Model Life" target="_blank">A Model Life (July 2012)</a></strong> - Production Manager</li>
+            </ul><!-- END WORK LIST -->
+          </li><!-- END VT EMPLOYER -->
+
+          <li>
+            <p>
+              <h3>Freelancer</h3> 2009 - Present
+            </p>
+            <p>
+              I reflect my role in Framestore as a generalist. Primarily I focus on web development. 
+              This role is self managed and In some cases I have employed team members to help on larger projects and managed them. 
+              Through Framestore i was able to apply for a vacancy Rathergood where hiring for. 
+              The budget was low and expectations were high as the Rathergood’s first feature length film starred the voice talents of Steve Coogan as narrator, 
+              plus Julian Barratt and Julia Davis. It was an exciting project and I approached each day with a smile, even around my day job. 
+              I recruited an After Effects freelancer to help out in which I personally managed. We achieved everything beyond expectation and the client was very happy.
+            </p>
+            <ul>
+              <li>
+                <strong><a href="http://www.unclewormsley.com/" title="UNCLE WORMSLEY'S CHRISTMAS" target="_blank">Uncle Wormsley’s Christmas</a></strong> 
+                - (VFX/Supervisor - After Effects)
+              </li>
+              <li>
+                <strong><a href="http://www.stuart-reed.com/" title="Stuart Reed website" target="_blank">Stuart Reed</a></strong> 
+                - (Developer - DreamWeaver/Javascript)
+              </li>
+              <li>
+                <strong><a href="http://velvettrumpet.co.uk/" title="Velvet Trumpet website" target="_blank">Velvet Trumpet's Website</a></strong> 
+                - (Developer - DreamWeaver/HTML/CSS/JQUERY)
+              </li>
+              <li>
+                <strong><a href="http://georginamackiefitness.co.uk/" title="Georgina Mackie website" target="_blank">Georgina Mackie Fitness Website</a></strong> 
+                - (Developer - DreamWeaver/HTML/CSS/JQUERY)
+              </li>
+            </ul><!-- END WORK LIST -->
+          </li><!-- END FREELANCE EMPLOYER -->
+        </ul><!-- END EMPLOYERS LIST -->	
+      </li><!-- END EXPERIENCE SECTION IN LIST -->
+      <div class="contact">
+        <h2>References</h2>
+        <ul>
+          <li><strong>Mike Woods</strong> - Digital Producer at Framestore<br /><a href="mailto:mike.woods@framestore.com">mike.woods@framestore.com</a></li>
+          <li><strong>Simon Whalley</strong> - Executive Producer at Framestore<br /><a href="mailto:simon.whalley@framestore.com">simon.whalley@framestore.com</a></li>
+          <li><strong>Hanna Jane Price</strong> - HR Manager at Industrial Light &amp; Magic<br /><a href="mailto:hprice@ilm-vancouver.com">hprice@ilm-vancouver.com</a></li>
+          <li><strong>Joel Veitch</strong> - Founder of Rathergood.com<br /><a href="mailto:joel@rathergood.com">joel@rathergood.com</a></li>
+          <li><strong>Simon Gosling</strong> - Executive Producer at Framestore<br /><a href="mailto:sgosling@framestore.com">sgosling@framestore.com</a></li>
+        </ul>
+      </div>
+    </ul><!-- END INFORMATION CONTENT -->
+  </div><!-- END CONTENT -->
+    <footer>
+      <div class="footer">
+          <div class="button"><a target="_blank" href="http://www.samgregorydesigner.co.uk/SamGregoryCV-2012.pdf" title="download my web developer/producer">Download CV</a></div>
+          <div class="button"><a href="#" title="Take a look at my references">References</a></div>
+        </div>
+      </div>
+    </footer>
+</div><!-- END CONTAINER -->
+</body>
+
+<script type="text/javascript" async="" src="http://www.google-analytics.com/ga.js"></script>
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-27293381-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
+</html>
---
+++ scripts/document.js
@@ -0,0 +1,122 @@
+$(function() {
+
+	var tooltip = $('.tooltip'),
+		headerHeight = $('.header').height(),
+		touch = ('ontouchstart' in window) || (window.DocumentTouch && document instanceof DocumentTouch),
+		theWindow = $(window),
+		windowHeight = theWindow.height(),
+		theFooter = $('.footer-wrap');
+
+	if (touch) $('body').addClass('touch');
+			
+	$('.references, .contact-wrapper').click(function(e) {
+		if (!$(e.target).parents('.contact-card').length) {
+			e.preventDefault();
+			hideShow('contact-wrapper');
+		}
+	});
+
+	theWindow.resize(function() {
+		if ($('.contact-card').length) {
+			$('.contact-card').css('left', ($(window).width()/2) - ($('.contact-card').outerWidth()/2));
+		}
+	});
+
+	if (!touch) {
+		$('.job-list').on('hover', 'a[data-img]', function(e) {
+			var target = $(this),
+				img = target.data('img');
+
+			if (e.type === 'mouseenter') {
+				tooltip.find('img').attr('src', '../images/' + img + '.jpg');
+				target.parent().append(tooltip.show());
+
+				if ((tooltip.offset().top + tooltip.height()) > (theWindow.scrollTop() + theWindow.height())) tooltip.css({ 'bottom': 30, 'top': 'auto'})
+			}
+			else {
+				tooltip.removeAttr('style');
+				tooltip.find('img').attr('src', '../images/loading.gif');
+			}
+		});
+var t = setTimeout(function() {
+		theFooter.removeClass('show');
+	}, 2000);
+
+$(document).on('mousemove', function(e) {
+				if (e.clientY > (windowHeight - 200)) {
+					theFooter.addClass('show');
+				} else {
+					theFooter.removeClass('show');
+				}
+		});
+
+		
+	}
+	
+
+
+	$('.nav-wrap').on('click', 'a', function(e) {
+		e.preventDefault();
+
+		var target = $(this),
+			id = target.attr('href'),
+			localY = $(id).offset().top,
+			offset = theWindow.width() < 549 ? 0 : headerHeight;
+
+		$('body').animate({
+			scrollTop: localY - offset
+		});
+	});
+
+	theWindow.on('scroll', function() {
+		var currentpos = theWindow.scrollTop(),
+			body = $('.section');
+
+		$('.nav-wrap a').removeClass('active');
+				
+		body.each(function(i) {
+			var target = $(this),
+				top = target.position().top - (headerHeight + 20),
+				bottom = target.offset().top + (target.height() - 150),
+				index = target.index();
+
+			if (currentpos > top && currentpos < bottom) {
+				$('.nav-wrap li').eq(index).find('a').addClass('active');
+			}
+
+		});
+	}).trigger('scroll');
+
+	
+});
+
+function hideShow(divid) {
+
+	var target = $('.'+divid);
+
+		card = target.find('.contact-card');
+
+		target.toggle();
+
+		card.css({
+			left: ($(window).width()/2) - (card.outerWidth()/2),
+			top: ($(window).height()/2) - (card.outerHeight()/2),
+		});
+
+		// if (card.css('display', 'block')) {
+		// 	$('.references').css({
+		// 		'background': '#ccc',
+		// 		'color': '#fff',
+		// 	})
+		// } 
+		// if (card.css('display', 'none')) {
+		// 	console.log('hi')
+		// 	$('.references').removeAttr('style');
+		// }
+
+		// $('.contact-card ul').css('height', $(window).height() - ( $('.header').outerHeight() + $('.footer-wrap').outerHeight() ))
+
+		if ( card.height > ($(window).height() - ( $('.header').height() + $('.footer-wrap').height() )) ) {
+			console.log('hi')
+		}
+}
