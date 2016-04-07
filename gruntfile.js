ec2195efd239425860fa1aa2cc795f5e0606f7bd
Sam Gregory <samuel@digital-detox.co.uk>
2016-04-07 07:39:17 +0100

    new design

--- gruntfile.js
+++ gruntfile.js
@@ -64,7 +64,7 @@ module.exports = function(grunt) {
         tasks: ['assemble']
       },
       images: {
-        files: ['images/**/*'],
+        files: ['components/images/**/*'],
         tasks: ['copy:images']
       }
     },
@@ -72,28 +72,50 @@ module.exports = function(grunt) {
     copy: {
       main: {
         files: [
-          // flattens results to a single level
           {expand: true, flatten: true, src: ['node_modules/bootstrap-sass/assets/fonts/bootstrap/**/*'], dest: 'css/fonts', filter: 'isFile'},
         ],
       },
       images: {
-        files: [{
-          src: 'images/**/*',
-          dest: 'dist/'
-        }] 
+        expand: true,
+        flatten: true,
+        cwd: 'components/images/',
+        src: '*.{jpg,gif,png}',
+        dest: 'dist/images/'
       },
       fonts: {
-        files: [{
-          src: 'css/fonts/**/*',
-          dest: 'dist/css/fonts',
-          expand: true, 
-          flatten: true
-        }]
+        src: 'css/fonts/**/*',
+        dest: 'dist/css/fonts',
+        expand: true, 
+        flatten: true
       },
       files : {
+        src: ['*.pdf'],
+        dest: 'dist/'
+      }
+    },
+
+    responsive_images: {
+      images: {
+        options: {
+          sizes: [
+          {
+            name: 'small',
+            width: '50%',
+            rename: false
+          },
+          {
+            name: "large",
+            width: '100%',
+            suffix: "@x2",
+            rename: false
+          }]
+        },
         files: [{
-          src: ['*.pdf'],
-          dest: 'dist/'
+          expand: true,
+          flatten: true,
+          src: ['*.{jpg,gif,png}'],
+          cwd: 'components/images/logos/',
+          dest: 'dist/images/'
         }]
       }
     },
@@ -152,29 +174,59 @@ module.exports = function(grunt) {
     },
 
     browserify: {
-         options: {
-            browserifyOptions: {
-               debug: true
-            }
-         },
-         dist: {
-            files: {
-               '.tmp/js/main.js': 'components/js/**/*.js'
-            }
-         },
-         dev: {
+       options: {
+          browserifyOptions: {
+             debug: true
+          }
+       },
+       dist: {
           files: {
-               'dist/js/main.js': 'components/js/**/*.js'
-            }
-         }
+             '.tmp/js/main.js': 'components/js/**/*.js'
+          }
+       },
+       dev: {
+        files: {
+             'dist/js/main.js': 'components/js/**/*.js'
+          }
+       }
+    },
+
+    imagemin: {                          // Task
+      dynamic: {                         // Another target
+        files: [{
+          expand: true,                  // Enable dynamic expansion
+          cwd: 'dist/images/',                   // Src matches are relative to this path
+          src: ['**/*.{png,jpg,gif}'],   // Actual patterns to match
+          dest: 'dist/images/'                  // Destination path prefix
+        }]
       }
+    }
   });
 
-  grunt.registerTask('default', 
+  grunt.registerTask('imageloop', function() {
+    var images = [];
+    grunt.file.recurse('components/images/logos', function(abspath, rootdir, subdir, filename) {
+      if (filename.match(/jpg|gif|png/)) {
+        var ext = filename.match(/jpg|gif|png/);
+        images.push(
+          { 
+            src: filename,
+            filename: filename.replace(/.(jpg|gif|png)/, ''),
+            ext: ext
+           }
+          );
+      }
+    });
+    grunt.file.write('components/data/work.json', JSON.stringify(images));
+  });
+
+  grunt.registerTask('default',
     [
-    'copy', 
+    'imageloop',
+    'copy',
+    'responsive_images',
     'browserify:dev',
-    'sass:dev',  
+    'sass:dev', 
     'assemble', 
     'connect',
     'watch'
@@ -183,15 +235,15 @@ module.exports = function(grunt) {
 
   grunt.registerTask('build', 
     [
-    'copy', 
+    'imageloop',
+    'copy:main', 'copy:fonts', 'copy:files',
+    'imagemin', 
     'browserify:dist',
     'uglify',
     'sass:build', 
     'autoprefixer', 
     'assemble',
-    'htmlmin',
-    'connect',
-    'watch'
+    'htmlmin'
     ]
   );
 }
